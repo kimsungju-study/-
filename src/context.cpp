5aaf59111b03c4558a84e601419549e94ba3576e
kimsungju <kimsungju4@naver.com>
2021-05-10 22:25:00 +0900

    WEEK 10_1

--- src/context.cpp
+++ src/context.cpp
@@ -1,4 +1,3 @@
-  
 #include "context.h"
 #include "image.h"
 #include <imgui.h>
@@ -127,20 +126,13 @@ uint32_t indices[] = {
     m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, 
     GL_STATIC_DRAW,indices, sizeof(uint32_t) * 36);
 
+    m_simpleProgram = Program::Create("./shader/simple.vs", "./shader/simple.fs");
+    if (!m_simpleProgram)
+    return false;
 
-
-    ShaderPtr vertShader = Shader::CreateFromFile("./shader/lighting.vs", GL_VERTEX_SHADER);
-    ShaderPtr fragShader = Shader::CreateFromFile("./shader/lighting.fs", GL_FRAGMENT_SHADER);
-    if (!vertShader || !fragShader)
-     return false;
-    SPDLOG_INFO("vertex shader id: {}", vertShader->Get());
-    SPDLOG_INFO("fragment shader id: {}", fragShader->Get());
-
-    m_program = Program::Create({fragShader, vertShader});
-     if (!m_program)
-         return false;
-    SPDLOG_INFO("program id: {}", m_program->Get());
-
+    m_program = Program::Create("./shader/lighting.vs", "./shader/lighting.fs");
+    if (!m_program)
+    return false;
 
     glClearColor(0.1f, 0.1f, 0.1f, 0.1f);
 		
@@ -155,6 +147,12 @@ uint32_t indices[] = {
 
     auto image2 = Image::Load("./image/awesomeface.png");
     m_texture2 = Texture::CreateFromImage(image2.get());
+
+
+    m_material.diffuse = Texture::CreateFromImage(
+        Image::Load("./image/container2.png").get());
+    m_material.specular = Texture::CreateFromImage(
+        Image::Load("./image/container2_specular.png").get());
 
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, m_texture->Get());
@@ -188,25 +186,21 @@ void Context::Render() {
         }
         if (ImGui::CollapsingHeader("light", ImGuiTreeNodeFlags_DefaultOpen)) {
             ImGui::DragFloat3("l.position", glm::value_ptr(m_light.position), 0.01f);
+            ImGui::DragFloat3("l.direction", glm::value_ptr(m_light.direction), 0.01f);
+            ImGui::DragFloat2("l.cutoff", glm::value_ptr(m_light.cutoff), 0.5f, 0.0f, 90.0f);
+            ImGui::DragFloat("l.distance", &m_light.distance, 0.5f, 0.0f, 3000.0f);
             ImGui::ColorEdit3("l.ambient", glm::value_ptr(m_light.ambient));
             ImGui::ColorEdit3("l.diffuse", glm::value_ptr(m_light.diffuse));
             ImGui::ColorEdit3("l.specular", glm::value_ptr(m_light.specular));
         }
     
         if (ImGui::CollapsingHeader("material", ImGuiTreeNodeFlags_DefaultOpen)) {
-            ImGui::ColorEdit3("m.ambient", glm::value_ptr(m_material.ambient));
-            ImGui::ColorEdit3("m.diffuse", glm::value_ptr(m_material.diffuse));
-            ImGui::ColorEdit3("m.specular", glm::value_ptr(m_material.specular));
             ImGui::DragFloat("m.shininess", &m_material.shininess, 1.0f, 1.0f, 256.0f);
         }
         ImGui::Checkbox("animation", &m_animation);
     }
             
     ImGui::End();
-
-    	
-   
-
 
     std::vector<glm::vec3> cubePositions = {
         glm::vec3( 0.0f, 0.0f, 0.0f),
@@ -228,37 +222,46 @@ void Context::Render() {
         glm::rotate(glm::mat4(1.0f), glm::radians(m_cameraYaw), glm::vec3(0.0f, 1.0f, 0.0f)) *
         glm::rotate(glm::mat4(1.0f), glm::radians(m_cameraPitch), glm::vec3(1.0f, 0.0f, 0.0f)) *
         glm::vec4(0.0f, 0.0f, -1.0f, 0.0f);
+        m_light.position = m_cameraPos;
+        m_light.direction = m_cameraFront;
+
 
     auto projection = glm::perspective(glm::radians(45.0f),
-        (float)m_width / (float)m_height, 0.01f, 100.0f);
-    	
-  
+        (float)m_width / (float)m_height, 0.01f, 30.0f);
     auto view = glm::lookAt(
         m_cameraPos,
         m_cameraPos + m_cameraFront,
         m_cameraUp);
-    auto lightModelTransform =
-        glm::translate(glm::mat4(1.0), m_light.position) *
-        glm::scale(glm::mat4(1.0), glm::vec3(0.1f));
-    m_program->Use();
-    m_program->SetUniform("light.position", m_light.position);
-    m_program->SetUniform("light.ambient", glm::vec3(1.0f));
-    m_program->SetUniform("material.ambient", m_material.ambient);
-    m_program->SetUniform("transform", projection * view * lightModelTransform);
-    m_program->SetUniform("modelTransform", lightModelTransform);
-    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
-    m_program->Use();	
+    //auto lightModelTransform =
+    //    glm::translate(glm::mat4(1.0), m_light.position) *
+    //    glm::scale(glm::mat4(1.0), glm::vec3(0.1f));
+    //m_simpleProgram->Use();
+    //m_simpleProgram->SetUniform("color", glm::vec4(m_light.ambient + m_light.diffuse, 1.0f));
+    //m_simpleProgram->SetUniform("transform", projection * view * lightModelTransform);
+    //glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+    m_program->Use();
     m_program->SetUniform("viewPos", m_cameraPos);
     m_program->SetUniform("light.position", m_light.position);
+    m_program->SetUniform("light.direction", m_light.direction);
+    m_program->SetUniform("light.cutoff", glm::vec2(
+        cosf(glm::radians(m_light.cutoff[0])),
+        cosf(glm::radians(m_light.cutoff[0] + m_light.cutoff[1]))));
+    m_program->SetUniform("light.attenuation",GetAttenuationCoeff(m_light.distance));    
     m_program->SetUniform("light.ambient", m_light.ambient);
     m_program->SetUniform("light.diffuse", m_light.diffuse);
     m_program->SetUniform("light.specular", m_light.specular);
-    m_program->SetUniform("material.ambient", m_material.ambient);
-    m_program->SetUniform("material.diffuse", m_material.diffuse);
-    m_program->SetUniform("material.specular", m_material.specular);
+    m_program->SetUniform("material.diffuse", 0);
+    m_program->SetUniform("material.specular", 1);
     m_program->SetUniform("material.shininess", m_material.shininess);
 
+    glActiveTexture(GL_TEXTURE0);
+    m_material.diffuse->Bind();
+    	
+    glActiveTexture(GL_TEXTURE1);
+    m_material.specular->Bind();
+ 
     for (size_t i = 0; i < cubePositions.size(); i++){
         auto& pos = cubePositions[i];
         auto model = glm::translate(glm::mat4(1.0f), pos);

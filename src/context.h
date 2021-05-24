e34fc62303e102f3e441ea865cf2d444c1a4da31
kimsungju <kimsungju4@naver.com>
2021-05-24 22:32:27 +0900

    week12
    advancedGL

--- src/context.h
+++ src/context.h
@@ -31,9 +31,6 @@ private:
     ProgramUPtr m_postProgram;
     float m_gamma {1.0f};
 
-
-    
-
     MeshUPtr m_box;
     MeshUPtr m_plane;
 
@@ -71,6 +68,18 @@ private:
 
      // framebuffer
     FramebufferUPtr m_framebuffer;
+
+     // cubemap
+    CubeTextureUPtr m_cubeTexture;
+    ProgramUPtr m_skyboxProgram;
+    ProgramUPtr m_envMapProgram;
+
+
+    TexturePtr m_grassTexture;
+    ProgramUPtr m_grassProgram;
+    std::vector<glm::vec3> m_grassPos;
+    BufferUPtr m_grassPosBuffer;
+    VertexLayoutUPtr m_grassInstance;
 
     int m_width {WINDOW_WIDTH};
     int m_height {WINDOW_HEIGHT};
